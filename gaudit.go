241c4a55af2d61e6501124bf4d6640832602b8dc
maxhasan882 <rhmithu50@gmail.com>
2024-10-05 06:33:32 +0600

    read me update

--- gaudit.go
+++ gaudit.go
@@ -2,9 +2,14 @@ package gaudit
 
 import (
 	_ "github.com/its-own/gaudit/internal/audit_log"
+	"github.com/its-own/gaudit/internal/hooks"
 	"log/slog"
 )
 
 func init() {
 	slog.Default().Info("hookiee in action")
+}
+
+func New() *hooks.DefaultHooks {
+	return hooks.NewDefaultHook()
 }
